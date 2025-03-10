26ebd05c9b6c9ef92438a2cdbb9d8b9c5154cc61
Christopher <mwanikigachina@gmail.com>
2025-03-10 19:25:03 +0300

    initialize Go Api

---
+++ cmd/api/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/Code-of-christopher/GoApi/internal/handlers"
+	log "github.com/sirupsen/logrus"
+)
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Code-of-christopher/GoApi
+
+go 1.24.0
